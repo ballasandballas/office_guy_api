0667b621d65c244fc4502218f809d73a91054830
Elhanan Ballas <eballas14@gmail.com>
2020-01-06 22:25:28 +0200

    Fixing an error in the API.

--- office_guy_api/models/response_accounting_documents_create_response.py
+++ office_guy_api/models/response_accounting_documents_create_response.py
@@ -105,14 +105,21 @@ class ResponseAccountingDocumentsCreateResponse(object):
         :param status: The status of this ResponseAccountingDocumentsCreateResponse.  # noqa: E501
         :type: str
         """
-        allowed_values = ["Success", "BusinessError", "TechnicalError"]  # noqa: E501
+        # Note: Fixed error in API, approved by Effi Teva
+        # allowed_values = ["Success", "BusinessError", "TechnicalError"]  # noqa: E501
+        value_map = {
+            0: "Success",
+            1: "BusinessError",
+            2: "TechnicalError"
+        }
+        allowed_values = list(value_map.keys())
         if status not in allowed_values:
             raise ValueError(
                 "Invalid value for `status` ({0}), must be one of {1}"  # noqa: E501
                 .format(status, allowed_values)
             )
 
-        self._status = status
+        self._status = value_map.get(status)
 
     @property
     def user_error_message(self):
